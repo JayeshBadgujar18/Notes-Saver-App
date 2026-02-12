3220ed180c5b3f5174fe6474a53c791d09f14f73
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-02-12 18:50:59 +0530

    fix: update Navbar layout for improved styling and responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row gap-4 p-4 text-blue-500 ">
+
+    <div className="w-full flex  justify-between items-center bg-gradient-to-r from-red-500 via-yellow-500 to-lime-500 shadow-md"> 
+
+        <div className="flex flex-row gap-4 p-4 text-blue-500 ">
       <NavLink to="/" className="hover:text-blue-300">
         Home
       </NavLink>
@@ -13,6 +16,12 @@ const Navbar = () => {
         Pastes
       </NavLink>
     </div>
+
+
+    </div>
+
+  
+ 
   )
 }
 
