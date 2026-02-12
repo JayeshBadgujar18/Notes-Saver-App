69f7c97683440b4797b6c97718337eb3e52cd8f3
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-02-12 23:12:25 +0530

    fix: update Navbar text color for better visibility and consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
 
     <div className="w-full flex  justify-between items-center bg-gradient-to-r from-red-500 via-yellow-500 to-lime-500 shadow-md"> 
 
-        <div className="flex flex-row gap-4 p-4 text-blue-500 ">
-      <NavLink to="/" className="hover:text-blue-300">
+        <div className="flex flex-row gap-4 p-4 text-black-500 ">
+      <NavLink to="/" className="hover:text-blue-900">
         Home
       </NavLink>
 
-      <NavLink to="/pastes" className="hover:text-blue-300">
+      <NavLink to="/pastes" className="hover:text-blue-900">
         Pastes
       </NavLink>
     </div>
